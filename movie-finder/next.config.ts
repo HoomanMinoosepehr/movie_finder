5ac6fdedf54739173861cc0e79b5871564eb11ef
Hooman Minoosepehr <hoomanminoosepehr@gmail.com>
2025-05-01 13:45:32 -0600

    change next setting to allow downloading image from external domain

--- movie-finder/next.config.ts
+++ movie-finder/next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
