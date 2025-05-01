190cd531d894e999d3a6546212b93c43d3265ac1
Hooman Minoosepehr <hoomanminoosepehr@gmail.com>
2025-05-01 13:58:38 -0600

    add a font family to the layout to use for the entire website

--- movie-finder/src/app/layout.tsx
+++ movie-finder/src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// import the Inter font family from google fonts for the entire app
+const inter = Inter({ subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Movie Finder",
@@ -23,9 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
-      <body>
+    <html lang="en">
+      <body className={inter.className}>
         {children}
       </body>
     </html>
