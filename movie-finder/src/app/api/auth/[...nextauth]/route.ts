a98bf739d485a8be256bf1bd8933f393d3065e08
Hooman Minoosepehr <hoomanminoosepehr@gmail.com>
2025-05-01 23:03:50 -0600

    install next-auth and create API route to sign-in users

---
+++ movie-finder/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,61 @@
+import NextAuth from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
+import { prisma } from "@/lib/prisma";
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    return null;
+                }
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email }
+                });
+                if (!user) {
+                    return null;
+                }
+                const isPasswordValid = await compare(credentials.password, user.password);
+
+                if (!isPasswordValid) {
+                    return null;
+                }
+
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                };
+            }
+        }),
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            if (token && session.user) {
+                session.user.id = token.id as string;
+            }
+            return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        }
+    },
+    pages: {
+        signIn: '/auth/signin',
+    },
+    session: {
+        strategy: "jwt",
+    }
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
