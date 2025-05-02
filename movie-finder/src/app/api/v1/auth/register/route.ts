fb9f3db1bbfd4a6888a5ab9fd4067f21acbbb89d
Hooman Minoosepehr <hoomanminoosepehr@gmail.com>
2025-05-01 23:17:49 -0600

    create API to register new user

---
+++ movie-finder/src/app/api/v1/auth/register/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse, NextRequest } from "next/server";
+import { hash } from "bcrypt";
+import { prisma } from "../../../../../../lib/prisma";
+
+export async function POST(request: NextRequest) {
+    try {
+        const { name, email, password } = await request.json();
+
+        const userExists = await prisma.user.findUnique({
+            where: { email },
+        });
+        // reject registration if user already exists
+        if (userExists) {
+            return NextResponse.json(
+                { error: "User already exists" },
+                { status: 400 }
+            );
+        }
+
+        const hashedPassword = await hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            }
+        });
+
+        return NextResponse.json({
+            user: { id: user.id, name: user.name, email: user.email }
+        })
+    } catch {
+        return NextResponse.json(
+            { error: "Registration failed" },
+            { status: 500 }
+        )
+    }
+}
