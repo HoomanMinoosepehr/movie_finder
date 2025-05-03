2aa228714e09358b3a90da7cf72560419ed0b356
Hooman Minoosepehr <hoomanminoosepehr@gmail.com>
2025-05-03 00:16:41 -0600

    install debounce library to automatically update the url with a delay after stop typing, update the search bar to use debounce

--- movie-finder/src/components/SearchBar.tsx
+++ movie-finder/src/components/SearchBar.tsx
@@ -1,13 +1,42 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import debounce from "lodash.debounce";
 
 export default function SearchBar() {
-    const [searchText, setSearchText] = useState("");
+    const searchParams = useSearchParams();
+    const [searchText, setSearchText] = useState(searchParams.get('query') || "");
+    const router = useRouter();
 
     const handleChangeOnSearchBar = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         setSearchText(value);
+        debounceFunction.current?.(value);
     }
+
+    const debounceFunction = useRef<ReturnType<typeof debounce> | null>(null);
+
+    // we need to update the query params and push to the new URL
+    // we're using debounce to do this only after the user stops typing for some time
+    // this is to avoid too many requests to the server
+    useEffect(() => {
+        debounceFunction.current = debounce((value: string) => {
+            const params = new URLSearchParams(searchParams.toString());
+
+            if (value && value.length >= 2) {
+                params.set('query', value);
+                router.push(`/search?${params.toString()}`);
+            } else if (value.length === 0) {
+                params.delete('query');
+                router.push(`/`);
+            }
+        }, 500);
+
+        // clean up pending executions when dependencies change
+        return () => {
+            debounceFunction.current?.cancel();
+        };
+    }, [searchParams, router]);
 
     return (
         <div>
